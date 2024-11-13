37fee13cde8d836237c8e4ae5a057686ea18d462
anton <antonio.kolesnikow@gmail.com>
2024-11-13 21:06:22 +0300

    task_4 almost done

--- Task_4/Task_4.c
+++ Task_4/Task_4.c
@@ -36,6 +36,92 @@ int IntInputx(int i, int j, int k) {
   return x;
 }
 
+int ***createArray(int n) {
+  int ***array = (int ***)malloc(n * sizeof(int ***));
+  for (int i = 0; i < n; ++i) {
+    array[i] = (int **)malloc(n * sizeof(int *));
+    for (int j = 0; j < n; ++j) {
+      array[i][j] = (int *)malloc(n * sizeof(int));
+    }
+  }
+  return array;
+}
+
+void freeArray(int ***array, int n) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      free(array[i][j]);
+    }
+    free(array[i]);
+  }
+  free(array);
+}
+
+void calculateMaxDiagonalSum(int ***array, int n) {
+  int max_sum = 0;
+  int max_diag_type = -1;
+
+  // Первая диагональ (0 тип) [0][0][0] -> [n-1][n-1][n-1]
+  int sum_main_diag1 = 0;
+  for (int i = 0; i < n; i++) {
+    sum_main_diag1 += array[i][i][i];
+  }
+  if (sum_main_diag1 > max_sum) {
+    max_sum = sum_main_diag1;
+    max_diag_type = 0;
+  }
+
+  // Обратная диагональ (1 тип) [n-1][0][0] -> [0][n-1][n-1]
+  int sum_reverse_diag1 = 0;
+  for (int i = 0; i < n; i++) {
+    sum_reverse_diag1 += array[n - i - 1][i][i];
+  }
+  if (sum_reverse_diag1 > max_sum) {
+    max_sum = sum_reverse_diag1;
+    max_diag_type = 1;
+  }
+
+  // Главная диагональ (2 тип) [0][0][n-1] -> [n-1][n-1][0]
+  int sum_main_diag2 = 0;
+  for (int i = 0; i < n; i++) {
+    sum_main_diag2 += array[i][i][n - i - 1];
+  }
+  if (sum_main_diag2 > max_sum) {
+    max_sum = sum_main_diag2;
+    max_diag_type = 2;
+  }
+
+  // Обратная диагональ (3 тип) [n-1][0][n-1] -> [0][n-1][0]
+  int sum_reverse_diag3 = 0;
+  for (int i = 0; i < n; i++) {
+    sum_reverse_diag3 += array[n - i - 1][i][n - i - 1];
+  }
+  if (sum_reverse_diag3 > max_sum) {
+    max_sum = sum_reverse_diag3;
+    max_diag_type = 3;
+  }
+
+  printf("Максимальная сумма диагонали: %d\n", max_sum);
+  switch (max_diag_type) {
+    case 0:
+      printf("Эта сумма принадлежит диагонали [0][0][0] -> [%d][%d][%d]\n",
+             n - 1, n - 1, n - 1);
+      break;
+    case 1:
+      printf("Эта сумма принадлежит диагонали [%d][0][0] -> [0][%d][%d]\n",
+             n - 1, n - 1, n - 1);
+      break;
+    case 2:
+      printf("Эта сумма принадлежит диагонали [0][%d][0] -> [%d][0][%d]\n",
+             n - 1, n - 1, n - 1);
+      break;
+    case 3:
+      printf("Эта сумма принадлежит диагонали [0][0][%d] -> [%d][%d][0]\n",
+             n - 1, n - 1, n - 1);
+      break;
+  }
+}
+
 int main() {
   intro();
   int n = -1;
@@ -47,13 +133,7 @@ int main() {
     }
   } while (n < 0);
 
-  int ***array = (int ***)malloc(n * sizeof(int **));
-  for (int i = 0; i < n; ++i) {
-    array[i] = (int **)malloc(n * sizeof(int *));
-    for (int j = 0; j < n; ++j) {
-      array[i][j] = (int *)malloc(n * sizeof(int));
-    }
-  }
+  int ***array = createArray(n);
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
@@ -70,45 +150,8 @@ int main() {
       }
     }
   }
-  
-  int max_sum = 0;
-  int max_diag_type = -1;
-  // Первая диагональ (0 тип) [0][0][0] -> [n-1][n-1][n-1]
-  int sum_main_diag = 0;
-  for (int i = 0; i < n; i++) {
-    sum_main_diag += array[i][i][i];
-  }
-  if (sum_main_diag > max_sum) {
-    max_sum = sum_main_diag;
-    max_diag_type = 0;
-  }
-  // Обратная диагональ (1 тип) [n-1][0][0] -> [0][n-1][n-1]
-  int sum_reverse_diag = 0;
-  for (int i = 0; i < n; i++) {
-    sum_reverse_diag += array[n - i - 1][i][i];
-  }
-  if (sum_reverse_diag > max_sum) {
-    max_sum = sum_reverse_diag;
-    max_diag_type = 1;
-  }
 
-  printf("Максимальная сумма диагонали: %d\n", max_sum);
-  if (max_diag_type == 0) {
-    printf(
-        "Эта сумма принадлежит главной диагонали [0][0][0] -> [%d][%d][%d]\n",
-        n - 1, n - 1, n - 1);
-  } else if (max_diag_type == 1) {
-    printf(
-        "Эта сумма принадлежит обратной диагонали [%d][0][0] -> [0][%d][%d]\n",
-        n - 1, n - 1, n - 1);
-  }
-
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < 0; ++j) {
-      free(array[i][j]);
-    }
-    free(array[i]);
-  }
-  free(array);
+  calculateMaxDiagonalSum(array, n);
+  freeArray(array, n);
   return 0;
 }
